c4bbf63acbfdee9bcc2bcf7aadb3ad2d2704eeed
Mahbub_SR_SQA <mahbubqa@techetronventures.com>
2025-08-18 10:40:33 +0600

    Initial Commit

---
+++ MetroRent/MetroRentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroRentApp.swift
+//  MetroRent
+//
+//  Created by Techetron Ventures Ltd on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MetroRentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
